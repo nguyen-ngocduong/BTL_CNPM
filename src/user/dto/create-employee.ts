49d286cac3ee590be4f54c83c309709fb8576036
triuduongg <triuduongg374@gmail.com>
2025-04-18 11:46:28 +0700

    Delete src directory

--- src/user/dto/create-employee.ts
+++
@@ -1,8 +0,0 @@
-import { IsEnum } from 'class-validator';
-import { Role } from './../../enums/role.enum';
-import { CreateUserDto } from './create-user.dto';
-
-export class CreateEmployeeDto extends CreateUserDto {
-  @IsEnum(Role, { each: true })
-  roles: Role[];
-}

25fcfdc3192a4f614492d1bd681f65d80184ea76
triuduongg <triuduongg374@gmail.com>
2025-05-07 21:42:05 +0700

    Delete server directory

--- server/src/enums/orderStatus.enum.ts
+++
@@ -1,8 +0,0 @@
-export enum OrderStatus {
-  Processing = 'processing',
-  Delivering = 'delivering',
-  Delivered = 'delivered',
-  Return = 'return',
-  Refund = 'refund',
-  Cancel = 'cancel',
-}
--- server/src/enums/role.enum.ts
+++
@@ -1,6 +0,0 @@
-export enum Role {
-  User = 'user',
-  Admin = 'admin',
-  Manager = 'manager',
-  Employee = 'employee',
-}
